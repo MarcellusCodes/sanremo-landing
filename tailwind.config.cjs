50aa36ad9f5f5489940559c6fb93180fda7cfc80
MarcellusCodes <MarcellusCodes@gmail.com>
2022-07-03 21:41:17 +0200

    Added Branding, Fonts, Color theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FFFFFF",
+        secondary: "#F4E0C7",
+        tertiary: "#E22333",
+      },
+      fontFamily: {
+        primary: ["Bellota", "cursive"],
+        secondary: ["Lato", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
