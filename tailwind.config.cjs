aed0edaf38f4827d37c53334387e4f839425e74a
MarcellusCodes <MarcellusCodes@gmail.com>
2022-07-10 19:22:00 +0200

    Added Theme, Darkmode, Header, Navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: {
-        primary: "#FFFFFF",
-        secondary: "#F4E0C7",
-        tertiary: "#E22333",
-      },
       fontFamily: {
-        primary: ["Bellota", "cursive"],
-        secondary: ["Lato", "sans-serif"],
+        primary: ["Lato", "sans-serif"],
+        secondary: ["Martel Sans", "sans-serif"],
       },
     },
   },
